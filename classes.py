9432864c48f1c64c16114ffa41ff33fb6991c461
Samyak Jain <wsamyakjain@gmail.com>
2022-08-26 08:09:32 +0530

    added OOps concept

--- classes.py
+++ classes.py
@@ -1,3 +1,4 @@
+"""
 class emp:
     '''doc string (This is a employee class)'''
     def __init__(self):
@@ -17,7 +18,7 @@ class student:
     '''This is a student class'''
     def __init__(self,name,add):
         self.name = name
-        self.add = add
+        self.add = add 
     def info(self):
         print("Name is = ",self.name)
         print("Address is - ",self.add)
@@ -27,5 +28,28 @@ class student:
 s1 = student("samyak","bharat")
 s2 = student("krishna","mathura")
 
-s1.info()
+s1.info()  # equivalent to student.info(s1)
 s2.info()
+
+#a function is something which can be called independently but method needs to be called by class
+# hence they're dependent to be called upon.
+
+#no matters what name you passed it will act as a self
+class office:
+    def __init__(c,i):
+        c.i = i
+    
+    def info(i):
+        print(i.i)
+
+o1 = office(1)
+o1.info()
+
+"""
+
+class boom:
+    def display():
+        print("hey")
+
+b1 = boom()
+b1.display()
